aa2836963e3caaed40f21b219fb645f23259a667
mahmoud2391997 <mahmoudmelsaid1@gmail.com>
2022-11-23 22:53:59 +0200

    HomeScreen

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -11,7 +11,7 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(child: Text("home")),
+      body: Center(child: Text("Parent home screen")),
     );
   }
 }
